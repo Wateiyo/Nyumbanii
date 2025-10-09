dd34c7dd33136abfcd75bd52be44d35223ef5530
Eli-KE <119295051+Eli-KE@users.noreply.github.com>
2025-10-09 15:27:49 +0300

    add storage to firebase.config

--- frontend/src/config/firebase.js
+++ frontend/src/config/firebase.js
@@ -3,6 +3,8 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getAnalytics } from "firebase/analytics";
+import { getStorage } from 'firebase/storage';
+
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -24,6 +26,7 @@ const analytics = getAnalytics(app);
 // Initialize Firebase Authentication and export it
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+export const storage = getStorage(app); 
 export const googleProvider = new GoogleAuthProvider();
 
 export default app;
